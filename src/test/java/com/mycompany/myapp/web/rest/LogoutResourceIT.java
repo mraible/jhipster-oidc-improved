0738adae1075b5ab64be68522d10b49f00749f56
Matt Raible <matt.raible@okta.com>
2019-03-15 20:14:19 -0600

    Fix test

--- src/test/java/com/mycompany/myapp/web/rest/LogoutResourceIT.java
+++ src/test/java/com/mycompany/myapp/web/rest/LogoutResourceIT.java
@@ -1,40 +1,33 @@
 package com.mycompany.myapp.web.rest;
 
 import com.mycompany.myapp.JhipsterApp;
-import com.mycompany.myapp.config.Constants;
 import com.mycompany.myapp.security.AuthoritiesConstants;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.core.oidc.OidcIdToken;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestFilter;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.time.Instant;
-import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalAmount;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -49,7 +42,7 @@ public class LogoutResourceIT {
     private ClientRegistrationRepository registrations;
 
     @Autowired
-    private MappingJackson2HttpMessageConverter jacksonMessageConverter;
+    private WebApplicationContext context;
 
     private final static String ID_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9" +
         ".eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsIm" +
@@ -60,35 +53,34 @@ public class LogoutResourceIT {
     private MockMvc restLogoutMockMvc;
 
     @Before
-    public void before() {
-        LogoutResource logoutResource = new LogoutResource(registrations);
-        this.restLogoutMockMvc = MockMvcBuilders.standaloneSetup(logoutResource)
-            .setMessageConverters(jacksonMessageConverter).build();
-    }
-
-    @Test
-    public void getLogoutInformation() throws Exception {
-
+    public void before() throws Exception {
         Map<String, Object> claims = new HashMap<>();
         claims.put("groups", "ROLE_USER");
         claims.put("sub", 123);
         OidcIdToken idToken = new OidcIdToken(ID_TOKEN, Instant.now(),
             Instant.now().plusSeconds(60), claims);
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken(idToken));
+        SecurityContextHolderAwareRequestFilter authInjector = new SecurityContextHolderAwareRequestFilter();
+        authInjector.afterPropertiesSet();
 
-        String logoutUrl = this.registrations.findByRegistrationId("oidc").getProviderDetails()
-            .getConfigurationMetadata().get("end_session_endpoint").toString();
-        restLogoutMockMvc.perform(post("/api/logout")
-            .with(authentication(createMockOAuth2AuthenticationToken(idToken))))
-            .andExpect(status().isOk())
-            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
-            .andExpect(jsonPath("$.logoutUrl").value(logoutUrl));
+        this.restLogoutMockMvc = MockMvcBuilders.webAppContextSetup(this.context).build();
     }
 
-    private OAuth2AuthenticationToken createMockOAuth2AuthenticationToken(OidcIdToken idToken) {
+    @Test
+    public void getLogoutInformation() throws Exception {
+        String logoutUrl = this.registrations.findByRegistrationId("oidc").getProviderDetails()
+            .getConfigurationMetadata().get("end_session_endpoint").toString();
+        restLogoutMockMvc.perform(post("/api/logout"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$.logoutUrl").value(logoutUrl))
+            .andExpect(jsonPath("$.idToken").value(ID_TOKEN));
+    }
+
+    private OAuth2AuthenticationToken authenticationToken(OidcIdToken idToken) {
         Collection<GrantedAuthority> authorities = new ArrayList<>();
         authorities.add(new SimpleGrantedAuthority(AuthoritiesConstants.USER));
         OidcUser user = new DefaultOidcUser(authorities, idToken);
-
         return new OAuth2AuthenticationToken(user, authorities, "oidc");
     }
 }
